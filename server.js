be4a4f1c7d294ad44f21dbc630a54c4658c32ae7
Glitch (calm-iguana) <none>
2019-01-13 10:24:50 +0000

    🍂🌿 Checkpoint ./server.js:2672954/77

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get('/api/timestamp/:date_string?', validateString, ha );
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
