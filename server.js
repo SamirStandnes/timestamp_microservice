2497a3e9279b6e1716da22c5d3da4bd678e8b0f9
Glitch (calm-iguana) <none>
2019-01-27 16:18:20 +0100

    update readme and clean up

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,71 +19,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-/*
-const validateString = (err, req, res, next) => {
-let date = (req.params.date_string === '')? new Date(Date.now()) : new Date(req.params.date_string);
-
-  if(date) {
-    next(err);
-  }
-  else {
-    next();
-  }
-
-};
-*/
-
-/*
-const string_validator = (err, req, res, next) => {
-  
-  let req_param = req.params.date_string;
-  let date = req_param === undefined ? Date.now() : req_param;
-  if (!new Date(date)) { 
-    next(err);
-  }
-  next();
-  
-};
-
-*/
-
 const handler = (req, res, next) => {
   let req_param = req.params.date_string;
   let date = req_param === undefined ? new Date(Date.now()) : new Date(req_param);
   console.log(date);
-  if (date == "Invalid Date") { 
+  if (date == "Invalid Date") {
     res.json({error: "Invalid Date" });
   }
-  
+
   else {
   res.json({
     unix: date.getTime(),
     utc: date.toUTCString(),
   });
   }
-  
+
 };
 
-
 app.get('/api/timestamp/:date_string?', handler);
-
-/*
-app.use((err,req,res,next) => {
-  
-  const status = err.status || 500;
-  
-  res.status(status).json({"error" : "Invalid Date" });
-
-});
-
-*/
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
